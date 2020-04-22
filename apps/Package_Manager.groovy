798e18ce955093b0332d85501ea0195f5e1a18a5
dcmeglio <dmeglio@gmail.com>
2020-04-22 13:34:03 -0400

    View apps screen now shows version numbers
    Closes #65

--- apps/Package_Manager.groovy
+++ apps/Package_Manager.groovy
@@ -1641,10 +1641,10 @@ def prefPkgView() {
 			str += " <a href='${pkg.value.communityLink}' target='_blank'>Community Thread</a>"
 		str += "<ul>"
 		for (app in pkg.value.apps?.sort { it -> it.name}) {
-			str += "<li>${app.name} (app)</li>"
+			str += "<li>${app.name} v${app.version ?: pkg.value.version} (app)</li>"
 		}
 		for (driver in pkg.value.drivers?.sort { it -> it.name}) {
-			str += "<li>${driver.name} (driver)</li>"
+			str += "<li>${driver.name} v${driver.version ?: pkg.value.version} (driver)</li>"
 		}
 		str += "</ul></li>"
 	}
